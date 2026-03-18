ecf1262b6a5dd181d50cc653c682d5c280e41f68
Nyakio Maina <carolinenyakio21@gmail.com>
2026-03-18 21:19:10 +0300

    fixes

--- submission/02.sh
+++ submission/02.sh
@@ -1,2 +1,2 @@
 # Write the bitcoin cli command to get the bitcoin node network name
-bitcoin-cli -regtest getblockchaininfo
+bitcoin-cli -regtest getblockchaininfo | python3 -c "import json,sys; print(json.load(sys.stdin)['chain'])"
