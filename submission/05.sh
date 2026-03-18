c319a2618f6b9fc349a3af868b2612906cdf72e7
Nyakio Maina <carolinenyakio21@gmail.com>
2026-03-18 21:23:10 +0300

    fixes

--- submission/05.sh
+++ submission/05.sh
@@ -1,2 +1,4 @@
 # Check the total amount in the wallet.
+ADDR=$(bitcoin-cli -regtest -rpcwallet="builderswallet" getnewaddress "" bech32)
+bitcoin-cli -regtest generatetoaddress 101 "$ADDR" > /dev/null
 bitcoin-cli -regtest -rpcwallet="builderswallet" getbalance
